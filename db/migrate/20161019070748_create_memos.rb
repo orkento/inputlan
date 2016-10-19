6467f6742196cf8b69c919e7f984f6b708558bdd
Kento Oriya <k-oriya@mksc.jp>
2016-10-19 16:30:01 +0900

    null制約追加

--- db/migrate/20161019070748_create_memos.rb
+++ db/migrate/20161019070748_create_memos.rb
@@ -1,9 +1,9 @@
 class CreateMemos < ActiveRecord::Migration[5.0]
   def change
     create_table :memos do |t|
-      t.text :comment
-      t.string :writer
-      t.date :start_at
+      t.text :comment, null: false
+      t.string :writer, null: false
+      t.date :start_at, null: false
 
       t.timestamps
     end
